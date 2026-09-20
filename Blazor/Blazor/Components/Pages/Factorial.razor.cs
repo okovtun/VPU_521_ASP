80e5e753f21e3ce3fabae27b853eb224f7a0bb9f
User <User@DESKTOP-QHG18FL>
2026-09-20 12:45:33 +0300

    Lesson 2 - Factorial DONE

---
+++ Blazor/Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.NetworkInformation;
+using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Factorial
+	{
+		int number = 0;
+		BigInteger factorial = 1;
+		void Calculate()
+		{
+			factorial = 1;
+			for (int i = 1; i <= number; i++)
+			{
+				factorial *= i;
+			}
+		}
+	}
+}
